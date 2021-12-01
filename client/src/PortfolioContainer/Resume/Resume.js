d18fbcd056dd875bcd793a6881821c34da7afc05
Haley Yerxa <haley@heluxsystems.com>
2021-12-01 09:43:35 -0500

    changes to resume responsiveness

--- client/src/PortfolioContainer/Resume/Resume.js
+++ client/src/PortfolioContainer/Resume/Resume.js
@@ -56,6 +56,9 @@ const Resume = (props) => {
         { label: "Interests", logoSrc: <SportsGolfIcon className="bullet-logo"/> },
     ];
 
+    /* Array for id's*/
+    const idArray = ["Education", "Current Employment", "Work Experience", "Projects", "Interests"]
+
     const projectsDetails = [
         {
             title: "Senior Design Project - Auto Test Fixture ",
@@ -83,176 +86,198 @@ const Resume = (props) => {
     ];
 
     const resumeDetails = [
-        <div className="resume-screen-container" key="education">
-            <ResumeHeading
-                heading={"Florida Gulf Coast University"}
-                subHeading={"BACHELOR OF SCIENCE SOFTWARE ENGINEERING"}
-                fromDate={"2018"}
-                toDate={"Present"}
-            />
-            <ResumeHeading
-                heading={"High School "}
-                subHeading={"Hillcrest High School, Ottawa"}
-                fromDate={"2014"}
-                toDate={"2018"}
-            />
+        <div className="resume-item" key="education">
+            <div className="resume-screen-container" key="education" id="Education">
+                <ResumeHeading
+                    heading={"Florida Gulf Coast University"}
+                    subHeading={"BACHELOR OF SCIENCE SOFTWARE ENGINEERING"}
+                    fromDate={"2018"}
+                    toDate={"Present"}
+                />
+                <ResumeHeading
+                    heading={"High School "}
+                    subHeading={"Hillcrest High School, Ottawa"}
+                    fromDate={"2014"}
+                    toDate={"2018"}
+                />
+            </div>
         </div>,
 
         /* CURRENT EMPLOYMENT */
-        <div className="resume-screen-container" key="current-employment">
-            <div className="experience-container">
-                <ResumeHeading
-                    heading={"Helux Systems, INC"}
-                    subHeading={"Junior Data Scientist"}
-                    fromDate={"2021"}
-                    toDate={"Present"}
-                />
-                <div className="experience-description">
-                    <span className="resume-description-text">
-                        - Currently working as a fullstack web developer on a data migration tool
-                    </span>
-                    <br />
-                    <span className="resume-description-text">
-                        - Developing new features and creating bug fixes.
-                    </span>
-                    <br />
-                    <span className="resume-description-text">
-                        - Integrated the web app with backend services to enhance machine learning integration and data analytics
-                    </span>
-                    <br />
-                    <span className="resume-description-text">
-                        - Working in an Agile Environment, collaborating with other developers to achieve a common goal
-                    </span>
-                    <br />
+        <div className="resume-item" key="current-employment">
+            <div className="resume-screen-container" key="current-employment" id="Current Employment">
+                <div className="experience-container">
+                    <ResumeHeading
+                        heading={"Helux Systems, INC"}
+                        subHeading={"Junior Data Scientist"}
+                        fromDate={"2021"}
+                        toDate={"Present"}
+                    />
+                    <div className="experience-description">
+                        <span className="resume-description-text">
+                            - Currently working as a fullstack web developer on a data migration tool
+                        </span>
+                        <br />
+                        <span className="resume-description-text">
+                            - Developing new features and creating bug fixes.
+                        </span>
+                        <br />
+                        <span className="resume-description-text">
+                            - Integrated the web app with backend services to enhance machine learning integration and data analytics
+                        </span>
+                        <br />
+                        <span className="resume-description-text">
+                            - Working in an Agile Environment, collaborating with other developers to achieve a common goal
+                        </span>
+                        <br />
+                    </div>
                 </div>
-            </div>
-            <div className="experience-container">
-                <ResumeHeading
-                    heading={"Florida Gulf Coast University Computational Perception Laboratory"}
-                    subHeading={"Machine Learning Research Intern"}
-                    fromDate={"2021"}
-                    toDate={"Present"}
-                />
-                <div className="experience-description">
-                    <span className="resume-description-text">
-                        - Working with Dr. Chris DiMattina from the FGCU Department of Psychology on an NIH R-15
-                        grant from the National Eye Institute.
-                    </span>
-                    <br />
-                    <span className="resume-description-text">
-                        - Using Python TensorFlow and MATLAB to develop neural networks to model behavior of how
-                        humans perceive shadows and edges in images
-                    </span>
+                <div className="experience-container">
+                    <ResumeHeading
+                        heading={"Florida Gulf Coast University Computational Perception Laboratory"}
+                        subHeading={"Machine Learning Research Intern"}
+                        fromDate={"2021"}
+                        toDate={"Present"}
+                    />
+                    <div className="experience-description">
+                        <span className="resume-description-text">
+                            - Working with Dr. Chris DiMattina from the FGCU Department of Psychology on an NIH R-15
+                            grant from the National Eye Institute.
+                        </span>
+                        <br />
+                        <span className="resume-description-text">
+                            - Using Python TensorFlow and MATLAB to develop neural networks to model behavior of how
+                            humans perceive shadows and edges in images
+                        </span>
+                    </div>
                 </div>
-            </div>
-        </div>,
+                </div>
+            </div>,
 
         /* WORK EXPERIENCE */
-        <div className="resume-screen-container" key="work-experience">
-            <div className="experience-container">
-                <ResumeHeading
-                    heading={"Bell Canada"}
-                    subHeading={"Junior Software Developer"}
-                    fromDate={"May 2020"}
-                    toDate={"August 2020"}
-                />
-                <div className="experience-description">
-                    <span className="resume-description-text">
-                        - Developed web tool for displaying account details using a .NET core web API and React JS user
-                        interface, and deployed the tool using Docker.
-                    </span>
-                    <br />
-                    <span className="resume-description-text">
-                        - Primary developer on the project, responsible for both back-end and front-end development.
-                    </span>
-                    <br />
-                    <span className="resume-description-text">
-                        - Member of an AGILE development team, gained experience working in sprints and accurately
-                        estimating the required time for various tasks.
-                    </span>
-                    <br />
-                    <span className="resume-description-text">
-                        - Used test-driven development to modify existing APIs in .NET to add new endpoints required for
-                        the project.
-                    </span>
-                    <br />
-                    <span className="resume-description-text">
-                        - Implemented React-Redux to handle multiple API calls and error handling.
-                    </span>
-                    <br />
-                    <span className="resume-description-text">
-                        - Used Material UI components in React, as well as developing custom React Hooks and styles.
-                    </span>
-                    <br />
+        <div className='resume-item' key="work-experience">
+            <div className="resume-screen-container" key="work-experience" id="Work Experience">
+                <div className="experience-container">
+                    <ResumeHeading
+                        heading={"Bell Canada"}
+                        subHeading={"Junior Software Developer"}
+                        fromDate={"May 2020"}
+                        toDate={"August 2020"}
+                    />
+                    <div className="experience-description">
+                        <span className="resume-description-text">
+                            - Developed web tool for displaying account details using a .NET core web API and React JS user
+                            interface, and deployed the tool using Docker.
+                        </span>
+                        <br />
+                        <span className="resume-description-text">
+                            - Primary developer on the project, responsible for both back-end and front-end development.
+                        </span>
+                        <br />
+                        <span className="resume-description-text">
+                            - Member of an AGILE development team, gained experience working in sprints and accurately
+                            estimating the required time for various tasks.
+                        </span>
+                        <br />
+                        <span className="resume-description-text">
+                            - Used test-driven development to modify existing APIs in .NET to add new endpoints required for
+                            the project.
+                        </span>
+                        <br />
+                        <span className="resume-description-text">
+                            - Implemented React-Redux to handle multiple API calls and error handling.
+                        </span>
+                        <br />
+                        <span className="resume-description-text">
+                            - Used Material UI components in React, as well as developing custom React Hooks and styles.
+                        </span>
+                        <br />
+                    </div>
                 </div>
-            </div>
-            <div className="experience-container">
-                <ResumeHeading
-                    heading={"Blackberry"}
-                    subHeading={"Software Development Student"}
-                    fromDate={"May 2019"}
-                    toDate={"August 2019"}
-                />
-                <div className="experience-description">
-                    <span className="resume-description-text">
-                        - Created Machine Learning program attached to Django web service and AWS S3 buckets to process
-                        data and predict values.
-                    </span>
-                    <br />
-                    <span className="resume-description-text">
-                        - Maintained multiple servers to ensure proper communication of data being received from devices
-                    </span>
-                    <br />
-                    <span className="resume-description-text">
-                        - Developed valuable projects in Elastic Stack and Kibana to interpret and visualise data
+                <div className="experience-container">
+                    <ResumeHeading
+                        heading={"Blackberry"}
+                        subHeading={"Software Development Student"}
+                        fromDate={"May 2019"}
+                        toDate={"August 2019"}
+                    />
+                    <div className="experience-description">
+                        <span className="resume-description-text">
+                            - Created Machine Learning program attached to Django web service and AWS S3 buckets to process
+                            data and predict values.
+                        </span>
+                        <br />
+                        <span className="resume-description-text">
+                            - Maintained multiple servers to ensure proper communication of data being received from devices
+                        </span>
+                        <br />
+                        <span className="resume-description-text">
+                            - Developed valuable projects in Elastic Stack and Kibana to interpret and visualise data
 
-                    </span>
-                    <br />
-                    <span className="resume-description-text">
-                        - Worked with Spring MVC Web Service and created and modified Docker images and containers.
-                    </span>
-                    <br />
-                    <span className="resume-description-text">
-                        - Debugged and established new code in Python, Java, and Kotlin, on Ubuntu
-                    </span>
-                    <br />
+                        </span>
+                        <br />
+                        <span className="resume-description-text">
+                            - Worked with Spring MVC Web Service and created and modified Docker images and containers.
+                        </span>
+                        <br />
+                        <span className="resume-description-text">
+                            - Debugged and established new code in Python, Java, and Kotlin, on Ubuntu
+                        </span>
+                        <br />
+                    </div>
                 </div>
-            </div>
-        </div>,
+                </div>
+            </div>,
 
         /* PROJECTS */
-        <div className="resume-screen-container" key="projects">
-            {projectsDetails.map((projectsDetails, index) => (
-                <ResumeHeading
-                    key={index}
-                    heading={projectsDetails.title}
-                    subHeading={projectsDetails.subHeading}
-                    description={projectsDetails.description}
-                    fromDate={projectsDetails.duration.fromDate}
-                    toDate={projectsDetails.duration.toDate}
-                />
-            ))}
+        <div className="resume-item" key="projects">
+            <div className="resume-screen-container" key="projects" id="Projects">
+                {projectsDetails.map((projectsDetails, index) => (
+                    <ResumeHeading
+                        key={index}
+                        heading={projectsDetails.title}
+                        subHeading={projectsDetails.subHeading}
+                        description={projectsDetails.description}
+                        fromDate={projectsDetails.duration.fromDate}
+                        toDate={projectsDetails.duration.toDate}
+                    />
+                ))}
+            </div>
         </div>,
 
         /* Interests */
-        <div className="resume-screen-container" key="interests">
-            <ResumeHeading
-                heading="Golf"
-                description="I am an NCAA Division 1 Golfer, with several acolades including 2020 ASUN All-Academic Team, Champion of the 2021 Pat Bradley Invite, and co-captain of my team."
-            />
-            <ResumeHeading
-                heading="Music"
-                description="I play 8 different instruments, with my primary instrument being piano. The others include flute, clarinet, tenor saxophone, oboe, violin, percussion, and piccolo."
-            />
-            <ResumeHeading
-                heading="Traveling"
-                description="I thrive in exploring the world and all the different cultures, food, and history traveling has to offer."
-            />
+        <div className="resume-item" key="interests">
+            <div className="resume-screen-container" key="interests" id="Interests">
+                <ResumeHeading
+                    heading="Golf"
+                    description="I am an NCAA Division 1 Golfer, with several acolades including 2020 ASUN All-Academic Team, Champion of the 2021 Pat Bradley Invite, and co-captain of my team."
+                />
+                <ResumeHeading
+                    heading="Music"
+                    description="I play 8 different instruments, with my primary instrument being piano. The others include flute, clarinet, tenor saxophone, oboe, violin, percussion, and piccolo."
+                />
+                <ResumeHeading
+                    heading="Traveling"
+                    description="I thrive in exploring the world and all the different cultures, food, and history traveling has to offer."
+                />
+            </div>
         </div>,
     ];
 
     const handleCarousal = (index) => {
-        let offsetHeight = 500;
+        let tallestDiv = 0;
+        idArray.forEach((id, i) => {
+            console.log(id);
+            if (document.getElementById(id).offsetHeight > tallestDiv) {
+                tallestDiv = document.getElementById(id).offsetHeight;
+                console.log(id + " is now the tallest div with a height of " + tallestDiv);
+            }
+                
+        })
+        let offsetHeight = tallestDiv;
+        let root = document.documentElement;
+        root.style.setProperty('--resume-height', tallestDiv + "px");
+        root.style.setProperty('--two-resume-height', tallestDiv*2 + "px");
 
         let newCarousalOffset = {
             style: { transform: "translateY(" + index * offsetHeight * -1 + "px)" },
